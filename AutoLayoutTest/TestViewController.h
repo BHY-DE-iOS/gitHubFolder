8ff6741c270cf8480f93372a8bd45e564181a445
白浩洋 <17310019226@163.com>
2016-03-25 16:14:15 +0800

    xib三等分视图

---
+++ AutoLayoutTest/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  AutoLayoutTest
+//
+//  Created by mac on 16/3/21.
+//  Copyright © 2016年 maimaihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
